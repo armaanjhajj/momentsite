95804f9f90f95b1eedc9fcb239584fd4f3c3ffb9
Armaan <armaanjhajj05@gmail.com>
2025-09-18 17:55:07 -0400

    Replace logo on QR1 and QR2 pages with black logo

--- src/pages/QR1.jsx
+++ src/pages/QR1.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/Untitled-4 (1).png';
 import '../App.scss';
 
 function QR1() {
