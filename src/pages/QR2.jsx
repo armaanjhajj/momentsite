3287f7c638f3e0524748dc6d03b2f8fae4596253
Armaan Jhajj <166579534+armaanjhajj@users.noreply.github.com>
2025-09-19 21:21:54 -0400

    Increase Instagram SVG size to 56x56

--- src/pages/QR2.jsx
+++ src/pages/QR2.jsx
@@ -65,8 +65,8 @@ function QR2() {
                 {/* Inline Instagram SVG (no extra deps) */}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="28"
-                  height="28"
+                  width="56"
+                  height="56"
                   viewBox="0 0 24 24"
                   fill="currentColor"
                   aria-hidden="true"
