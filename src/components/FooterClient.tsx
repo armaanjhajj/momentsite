127a4fc20d0c15ad355362b53c3e5d101129542c
Armaan <armaanjhajj05@gmail.com>
2025-11-16 02:47:38 -0500

    Add Team link to footer navigation

--- src/components/FooterClient.tsx
+++ src/components/FooterClient.tsx
@@ -25,6 +25,7 @@ export default function FooterClient() {
           <a href="mailto:contact@havemoments.com" className="hover:text-white no-underline">Contact</a>
           <Link href="/safety" className="hover:text-white no-underline">Safety</Link>
           <Link href="/" className="hover:text-white no-underline">Home</Link>
+          <Link href="/team" className="hover:text-white no-underline">Team</Link>
           <Link href="/survey" className="hover:text-white no-underline">Survey</Link>
           <Link href="/apply" className="hover:text-white no-underline">Apply</Link>
         </nav>
